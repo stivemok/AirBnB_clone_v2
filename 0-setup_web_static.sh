d534803693ebca28377ecc4109f0858a6b20048a
stivemok <stivemok@gmail.com>
2023-07-05 13:27:12 -0700

    prepare web server

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -5,8 +5,8 @@ apt-get -y update
 apt-get install -y nginx
 mkdir -p /data/web_static/shared/
 mkdir -p /data/web_static/release/test/
-touch /data/web_static/release/test/index.html
-echo "Hello World again!" > /data/web_static/release/test/index.html
+# touch /data/web_static/release/test/index.html
+echo "Holberton School" > /data/web_static/release/test/index.html
 ln -sf /data/web_static/releases/test/ /data/web_static/current
 chown -hR ubuntu:ubuntu /data
 sed -i '38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
