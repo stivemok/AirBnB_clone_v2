439c1457d8f0563c7ac332e8feece43dfa038759
stivemok <stivemok@gmail.com>
2023-07-14 17:24:15 -0700

    prepare web server

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -14,5 +14,5 @@ echo '<html>
 </html>' > /data/web_static/releases/test/index.html
 ln -sf /data/web_static/releases/test/ /data/web_static/current
 chown -hR ubuntu:ubuntu /data
-sed -i '38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
+sed -i '10 i \\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
 service nginx restart
