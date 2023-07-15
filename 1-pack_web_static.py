09ebe63882183f1ffc440e8da0c7ca01e6a269d5
stivemok <stivemok@gmail.com>
2023-07-14 17:25:04 -0700

    Compress before sending

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -9,10 +9,10 @@ from datetime import datetime
 def do_pack():
     """Return the archive path if correctly generated
     Otherwise, return None"""
-    local("mkdir -p versions")
+    local("sudo mkdir -p versions")
     date = datetime.now().strftime('%Y%m%d%H%M%S')
     path = "versions/web_static_{}.tgz".format(date)
-    result = local("tar -cvzf {} web_static".format(path))
+    result = local("sudo tar -cvzf {} web_static".format(path))
     if result.succeeded:
         size = os.stat(path).st_size
         print("web_static packed: {} -> {}Bytes".format(path, size))
