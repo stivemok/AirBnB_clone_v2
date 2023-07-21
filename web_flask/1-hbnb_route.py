d31f73a1ed6910d78600a14c6f5b9b1383c60792
stivemok <stivemok@gmail.com>
2023-07-21 09:07:32 -0700

    HBNB

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Starts a Flask web application"""
+from flask import Flask
+# Create Flask application
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_hbnb():
+    return "Hello HBNB!"
+
+@app.route("/hbnb", strict_slashes=False)
+def display_hbnb():
+    return "HBNB"
+
+
+# Start Flask web application
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
