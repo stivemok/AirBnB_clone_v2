2f6ecc1ec6bde7d2d49781b770472203711d34d6
stivemok <stivemok@gmail.com>
2023-07-21 09:35:45 -0700

    Python is coll

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""Starts a Flask web application"""
+from flask import Flask
+from markupsafe import escape
+# Create Flask application
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_hbnb():
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def display_hbnb():
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def display_cText(text):
+    text = text.replace("_", " ")
+    return "C %s" % (text)
+
+
+@app.route("/python/", strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def display_pythonText(text="is cool"):
+    if text != "is cool":
+        text = text.replace("_", " ")
+    return "Python %s" % (text)
+
+
+# Start Flask web application
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
