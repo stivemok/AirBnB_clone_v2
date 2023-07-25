a2f3ceb35503cdac32d8338dc3c494ebbdf105c2
stivemok <stivemok@gmail.com>
2023-07-25 13:30:08 -0700

    Cities by state

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -3,21 +3,25 @@
 
 from flask import Flask, render_template
 from models import storage
-# from models.state import State
-
+import os
 app = Flask(__name__)
 
 
-@app.route("/cities_by_states", strict_slashes=False)
-def cities_by_states():
-    states = storage.all("State")
-    return render_template("8-citeis_by_states.html", states=states)
-
-
 @app.teardown_appcontext
-def teardown(exc):
+def handle_teardown(self):
+    """
+        method to handle teardown
+    """
     storage.close()
 
 
+@app.route('/cities_by_states', strict_slashes=False)
+def city_state_list():
+    """
+        method to render states from storage
+    """
+    states = storage.all('State').values()
+    return render_template("8-cities_by_states.html", states=states)
+
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000)
+        app.run(host='0.0.0.0', port=5000)
