6089e437a67a975e18c9ff25bf64dbf5cc698ab9
stivemok <stivemok@gmail.com>
2023-07-26 09:30:56 -0700

    States and State

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -1,37 +1,35 @@
 #!/usr/bin/python3
 """Starts a Flask web application"""
 from models import storage
-from models.state import State
-from os import environ
-from flask import Flask, render_template
+from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
 
+@app.route("/states", strict_slashes=False)
+def states():
+    """Displays an HTML page with a list of all States.
+    States are sorted by name.
+    """
+    states = storage.all("State")
+    return render_template("9-states.html", state=states)
+
+
+@app.route("/states/<id>", strict_slashes=False)
+def states_id(id):
+    """Displays an HTML page with info about <id>, if it exists."""
+    for state in storage.all("State").values():
+        if state.id == id:
+            return render_template("9-states.html", state=state)
+    return render_template("9-states.html")
+
+
 @app.teardown_appcontext
-def close_db(error):
-    """ Remove the current SQLAlchemy Session """
+def teardown(exc):
+    """Remove the current SQLAlchemy session."""
     storage.close()
 
 
-@app.route('/states_list', strict_slashes=False)
-def states_list():
-    """ displays a HTML page with a list of states """
-    states = storage.all(State).values()
-    states = sorted(states, key=lambda k: k.name)
-    return render_template('7-states_list.html', states=states)
-
-
-@app.route('/cities_by_states', strict_slashes=False)
-def cities_list():
-    """ displays a HTML page with a list of cities by states """
-    states = storage.all(State).values()
-    states = sorted(states, key=lambda k: k.name)
-    st_ct = []
-    for state in states:
-        st_ct.append([state, sorted(state.cities, key=lambda k: k.name)])
-    return render_template('8-cities_by_states.html', states=st_ct, h_1="States")
-
-
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host="0.0.0.0")
